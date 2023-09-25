08dea6174cd05741cd37a6471d2f3f8f9c17af6c
martinww9 <mosta@192.168.1.88>
2023-09-25 20:03:22 -0300

    agrega clase VentanaAgregar y se arregla consistencia de datos al llamar ventanaMenu

--- src/proyectocm/ProyectoCM.java
+++ src/proyectocm/ProyectoCM.java
@@ -5,6 +5,8 @@ public class ProyectoCM {
     public static void main(String[] args) {
         
         Sistema sistema = new Sistema();
+        VentanaMenu ventana = new VentanaMenu(sistema);
+        ventana.setVisible(true);
         sistema.mostrarMenu();
         
     }
